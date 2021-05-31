18d39b6cfa63197168b0a30387fc5aab19cab272
chuonglc <laccamchuong@gmail.com>
2021-05-31 23:50:42 +1000

    * update javadoc * comment unused code

--- MyGame/src/main/java/ass3/mygame/ItemCreation.java
+++ MyGame/src/main/java/ass3/mygame/ItemCreation.java
@@ -4,22 +4,32 @@ import java.util.ArrayList;
 
 
 /**
- * Write a description of class ItemCreation here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * The ItemCreation class uses to create all items in game 
+ * and contains them in an ArrayList of Item
+ * @author Cam Chuong, Margi Patel
+ * @version 1.0
+ * @see ArrayList
  */
 public class ItemCreation
 {
     
     private ArrayList<Item> allItemsInGame;
-    
+    /**
+     * Constructor of ItemCreation, 
+     * initialize the ArrayList allItemsInGame 
+     * and run method {@link #createItems() createItems}
+     */
     public ItemCreation()
     {       
         allItemsInGame = new ArrayList();
         createItems();
     }
     
+    /**
+     * Method to create items: excaliburSword, key, frontGateKey, armor. 
+     * All items are added into ArrayList allItemsInGame.
+     * @see Item
+     */
     public void createItems(){
         
         Item item1, item2, excaliburSword, key, frontGateKey, armor;
@@ -36,6 +46,12 @@ public class ItemCreation
         allItemsInGame.add(armor);
     }
    
+    /**
+     * Method to get item
+     * @param stringItem Name of the item that needs to find
+     * @return the item
+     * @see Item
+     */
     public Item getItem(String stringItem){
         Item itemToReturn = null;
         for(Item item: allItemsInGame){
